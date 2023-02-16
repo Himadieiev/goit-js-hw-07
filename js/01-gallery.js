ef073495f7eaf014035b52bab0f9a702c026c4fa
Ruslan Himadieiev <gimadejev86@gmail.com>
2023-02-16 18:05:26 +0200

    add

--- js/01-gallery.js
+++ js/01-gallery.js
@@ -1,4 +1,30 @@
 import { galleryItems } from './gallery-items.js';
 // Change code below this line
 
-console.log(galleryItems);
+const gallery = document.querySelector('.gallery');
+const listOfImages = createGallery(galleryItems);
+
+function createGallery (galleryItems){
+  return galleryItems.map(({ preview, original, description }) => {
+    return `<div class="gallery__item">
+    <img class="gallery__image" src="${preview}" data-src="${original}" alt="${description}" /></div>`;
+  })
+  
+    .join('');
+}
+
+gallery.insertAdjacentHTML("afterbegin", listOfImages);
+
+function onGalleryClick(event) {
+  if (event.target.nodeName !== "IMG" ) {
+    return;
+  }
+
+  const instance = basicLightbox.create(`
+    <img src="${event.target.dataset.src}" width="800" height="600">
+`)
+
+instance.show()
+}
+
+gallery.addEventListener('click', onGalleryClick);
